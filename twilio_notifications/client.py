50408bf29e545fc30589cc8a1783b8d6ee129ae7
Hunter Williams <hunter@westwilliams.com>
2023-11-22 15:17:52 -0500

    Auto formatting

--- twilio_notifications/client.py
+++ twilio_notifications/client.py
@@ -13,40 +13,41 @@ class EmptyEnvironmentVariables(Exception):
     # Custom exception raised when environment variables are not found
     def __init__(self):
         Exception.__init__(
-            self, 'One or more Twilio environment variables not found. (TWILIO_ACCOUNT_SID or TWILIO_AUTH_TOKEN or TWILIO_MSG_SERVICE_SID)')
+            self,
+            "One or more Twilio environment variables not found. (TWILIO_ACCOUNT_SID or TWILIO_AUTH_TOKEN or TWILIO_MSG_SERVICE_SID)",
+        )
 
 
 def get_client_credentials():
-    logger.debug('Attempting to load Twilio environment variables.')
+    logger.debug("Attempting to load Twilio environment variables.")
 
-    twilio_account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    twilio_auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-    twilio_msg_service_sid = os.getenv('TWILIO_MSG_SERVICE_SID')
+    twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    twilio_msg_service_sid = os.getenv("TWILIO_MSG_SERVICE_SID")
 
     empty_env_vars = []
 
     if not twilio_account_sid:
-        empty_env_vars.append('TWILIO_ACCOUNT_SID')
+        empty_env_vars.append("TWILIO_ACCOUNT_SID")
 
     if not twilio_auth_token:
-        empty_env_vars.append('TWILIO_AUTH_TOKEN')
+        empty_env_vars.append("TWILIO_AUTH_TOKEN")
 
     if not twilio_msg_service_sid:
-        empty_env_vars.append('TWILIO_MSG_SERVICE_SID')
+        empty_env_vars.append("TWILIO_MSG_SERVICE_SID")
 
     if empty_env_vars:
         for env_var in empty_env_vars:
-            logger.error(
-                f'Twilio environment variable {env_var} not found.')
+            logger.error(f"Twilio environment variable {env_var} not found.")
         raise EmptyEnvironmentVariables
 
-    logger.debug('Successfully loaded Twilio environment variables.')
+    logger.debug("Successfully loaded Twilio environment variables.")
     return (twilio_account_sid, twilio_auth_token, twilio_msg_service_sid)
 
 
 class TwilioClient:
     def __init__(self):
-        logger.debug('Attempting to initilize Twilio client.')
+        logger.debug("Attempting to initilize Twilio client.")
 
         (
             twilio_account_sid,
@@ -60,8 +61,8 @@ class TwilioClient:
             self.client = Client(twilio_account_sid, twilio_auth_token)
 
         except TwilioRestException:
-            logger.error('Failed to initilize Twilio client.')
+            logger.error("Failed to initilize Twilio client.")
             raise
 
         else:
-            logger.debug('Successfully initilized Twilio client.')
+            logger.debug("Successfully initilized Twilio client.")
