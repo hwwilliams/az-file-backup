f44707d51b7eb1e726f4337fae56ae00b78b3c6e
Hunter Williams <hunter@westwilliams.com>
2023-11-22 22:14:34 -0500

    Add health check function

--- upload/process.py
+++ upload/process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import json, logging, os
+import json, logging, os, requests
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient
 
@@ -10,6 +10,21 @@ logger.setLevel(logging.INFO)
 
 def format_log_message(self, message: str):
     return f"{self.storage_account_name}/{self.storage_container_name} - {message}"
+
+
+def health_check(request_url: str, data=None, failure=False):
+    try:
+        if failure:
+            request_url = request_url + "/fail"
+
+        if data:
+            requests.post(request_url, data=data)
+        else:
+            requests.post(request_url)
+
+    except requests.RequestException as e:
+        logger.error(f"Health check ping failed {str(e)}")
+        raise
 
 
 def get_upload_settings():
@@ -35,62 +50,57 @@ def get_upload_settings():
         logger.error(f"Upload settings file not found '{upload_settings_file_path}'")
         raise
 
-    upload_settings_keys = [
-        "directory_path",
-        "storage_account_name",
-        "storage_container_name",
-        "storage_url_suffix",
-    ]
-    if all(entry in upload_settings_dict for entry in upload_settings_keys):
-        logger.debug(
-            f"Successfully loaded upload settings from file '{upload_settings_file_path}'"
-        )
-
-        return upload_settings_dict
-
     else:
-        logger.error(
-            f"Missing upload settings key from file '{upload_settings_file_path}', expected keys '{upload_settings_keys}'"
-        )
+        upload_settings_keys = [
+            "directory_path",
+            "health_check_url",
+            "storage_account_name",
+            "storage_container_name",
+            "storage_url_suffix",
+        ]
+        if all(entry in upload_settings_dict for entry in upload_settings_keys):
+            logger.debug(
+                f"Successfully loaded upload settings from file '{upload_settings_file_path}'"
+            )
+
+            return upload_settings_dict
+
+        else:
+            logger.error(
+                f"Missing upload settings key from file '{upload_settings_file_path}', expected keys '{upload_settings_keys}'"
+            )
 
 
 def get_files_to_upload(self):
-    try:
-        files = []
-        for file_name in os.listdir(self.directory_path):
-            file_path = os.path.join(self.directory_path, file_name)
-            if os.path.isfile(file_path):
-                if os.stat(file_path).st_size < 10000000000:
-                    files.append({"name": file_name, "path": file_path})
-                else:
-                    logger.warning(
-                        format_log_message(
-                            self,
-                            f"File '{file_name}' is larger than 10GB, skipping upload",
-                        )
+    files = []
+    for file_name in os.listdir(self.directory_path):
+        file_path = os.path.join(self.directory_path, file_name)
+        if os.path.isfile(file_path):
+            if os.stat(file_path).st_size < 10000000000:
+                files.append({"name": file_name, "path": file_path})
+            else:
+                logger.warning(
+                    format_log_message(
+                        self,
+                        f"File '{file_name}' is larger than 10GB, skipping upload",
                     )
+                )
 
-        return files
-
-    except Exception as e:
-        logger.error(e)
+    return files
 
 
 def upload_blob(self, file: object, blob_client: BlobClient):
-    try:
-        if blob_client.exists():
-            logger.info(
-                format_log_message(
-                    self, f"Blob '{file['name']}' already exists, skipping upload"
-                )
+    if blob_client.exists():
+        logger.info(
+            format_log_message(
+                self, f"Blob '{file['name']}' already exists, skipping upload"
             )
-        else:
-            logger.info(format_log_message(self, f"Uploading blob '{file['name']}'"))
-            with open(file=file["path"], mode="rb") as data:
-                blob_client.upload_blob(data)
+        )
 
-    except Exception as e:
-        logger.error(e)
+    else:
+        logger.info(format_log_message(self, f"Uploading blob '{file['name']}'"))
+        with open(file=file["path"], mode="rb") as data:
+            blob_client.upload_blob(data)
 
 
 class Process:
@@ -99,6 +109,7 @@ class Process:
 
         upload_settings = get_upload_settings()
         self.directory_path = upload_settings["directory_path"]
+        self.health_check_url = upload_settings["health_check_url"]
         self.storage_account_name = upload_settings["storage_account_name"]
         self.storage_container_name = upload_settings["storage_container_name"]
         self.storage_url_suffix = upload_settings["storage_url_suffix"]
@@ -106,7 +117,7 @@ class Process:
     def upload(self):
         try:
             storage_account_url = (
-                f"https://{self.storage_account_name}.{self.storage_url_suffix}"
+                f"httpsd://{self.storage_account_name}.{self.storage_url_suffix}"
             )
             blob_service_client = BlobServiceClient(
                 storage_account_url, credential=DefaultAzureCredential()
@@ -123,4 +134,8 @@ class Process:
                 upload_blob(self, file, blob_client)
 
         except Exception as e:
+            health_check(self.health_check_url, str(e), failure=True)
             logger.error(e)
+
+        else:
+            health_check(self.health_check_url)
