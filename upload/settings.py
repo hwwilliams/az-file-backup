1f4230287942677e616d267815b2c3785345d87a
Hunter Williams <hunter@westwilliams.com>
2023-12-07 00:38:25 -0500

    move settings to class

---
+++ upload/settings.py
@@ -0,0 +1,99 @@
+import os
+import pathlib
+import json
+import logging
+from pydantic import BaseModel, Field, ValidationError
+from typing import List
+from hashlib import md5
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class FileSizeError(Exception):
+    pass
+
+
+class File:
+    def __init__(self, path: str, name: str = None) -> None:
+        self.path = path
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.path
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @path.setter
+    def path(self, path: str) -> None:
+        if not os.path.exists(path):
+            raise FileNotFoundError(path)
+        if os.stat(path).st_size >= 10000000000:  # 10 GB
+            raise FileSizeError(path)
+        self._path = path
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        if name is None:
+            name = os.path.basename(self.path)
+        self._name = name
+
+    def get_md5(self) -> str:
+        with open(self.path, "rb") as f:
+            file_hash = md5()
+            chunk = f.read(8192)
+            while chunk:
+                file_hash.update(chunk)
+                chunk = f.read(8192)
+        return file_hash.digest()
+
+
+class UploadDefinition(BaseModel):
+    health_check_url: str | None
+    paths: List[str]
+    storage_account_name: str
+    storage_container_name: str
+    storage_url_suffix: str = "blob.core.windows.net"
+
+
+class Settings:
+    def __init__(self, *upload_definition: UploadDefinition) -> None:
+        self.upload_definition = upload_definition
+
+    @classmethod
+    def file(cls):
+        project_root_directory = pathlib.Path(pathlib.Path(__file__).parent).parent
+        settings_file = File(
+            os.path.join(project_root_directory, "settings", "settings.json")
+        )
+
+        try:
+            with open(settings_file.path, "r") as f:
+                loaded_definitions = json.load(f)
+        except json.JSONDecodeError:
+            logger.error(f"File contains invalid JSON '{settings_file.path}'")
+            raise
+        else:
+            return cls(*loaded_definitions)
+
+    @property
+    def upload_definition(self) -> List[UploadDefinition]:
+        return self._upload_definition
+
+    @upload_definition.setter
+    def upload_definition(self, upload_definition: UploadDefinition) -> None:
+        definitions = []
+        for definition in upload_definition:
+            if not UploadDefinition.model_validate(definition):
+                raise ValidationError
+
+            # Remove duplicates while preserving order
+            definition["paths"] = list(dict.fromkeys(definition["paths"]))
+            definitions.append(UploadDefinition(**definition))
+        self._upload_definition = definitions
--- upload/upload.py
+++ upload/upload.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 
-import json
 import logging
 import os
 import requests
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContentSettings
-from hashlib import md5
-from pydantic import BaseModel, Field, ValidationError
+from upload.settings import File, Settings
 from typing import List
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def health_check(request_url: str, data=None):
+def health_check(request_url: str, data=None) -> None:
     try:
         if data:
             requests.post(request_url, data=data)
@@ -25,74 +23,19 @@ def health_check(request_url: str, data=None):
         raise
 
 
-def get_upload_definition_settings():
-    try:
-        upload_settings_file_path = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "..", "settings", "settings.json")
-        )
-        logger.debug(
-            f"Attempting to load upload settings file '{upload_settings_file_path}'"
-        )
-        with open(upload_settings_file_path, "r") as f:
-            upload_definition_settings = json.load(f)
-            UploadDefinitionList.model_validate(upload_definition_settings)
-    except ValidationError:
-        logger.error(
-            f"Invalid JSON found when attempting to load upload settings file '{upload_settings_file_path}'"
-        )
-        raise
-    except json.JSONDecodeError:
-        logger.error(
-            f"No valid JSON found when attempting to load upload settings file '{upload_settings_file_path}'"
-        )
-        raise
-    except FileNotFoundError:
-        logger.error(f"Upload settings file not found '{upload_settings_file_path}'")
-        raise
-    else:
-        return upload_definition_settings["upload_definitions"]
-
-
-def is_large_file(file_path: str, file_name: str):
-    if os.stat(file_path).st_size >= 10000000000:
-        logger.warning(f"File '{file_name}' is larger than 10GB, skipping upload")
-        return True
-    else:
-        return False
-
-
-def get_file_md5(file_path: str):
-    try:
-        with open(file_path, "rb") as f:
-            file_hash = md5()
-            chunk = f.read(8192)
-            while chunk:
-                file_hash.update(chunk)
-                chunk = f.read(8192)
-    except FileNotFoundError:
-        logger.error(f"File not found '{file_path}'")
-        raise
-    except Exception as e:
-        logger.error(e)
-        raise
-    else:
-        return file_hash.digest()
-
-
 def get_files_to_upload(paths: List[str]):
     files = []
     for path in paths:
         for root, _, file_names in os.walk(os.path.abspath(path)):
             for file_name in file_names:
-                file_path = os.path.join(root, file_name)
-                if not is_large_file(file_path, file_name):
-                    files.append(
-                        {
-                            "name": file_name,
-                            "path": file_path,
-                            "md5_digest": get_file_md5(file_path),
-                        }
-                    )
+                file = File(os.path.join(root, file_name), file_name)
+                files.append(
+                    {
+                        "name": file.name,
+                        "path": file.path,
+                        "md5_digest": file.get_md5(),
+                    }
+                )
     return files
 
 
@@ -136,47 +79,27 @@ class DuplicateItemInListError(Exception):
     pass
 
 
-class UploadDefinition(BaseModel):
-    health_check_url: str
-    paths: List[str]
-    storage_account_name: str
-    storage_container_name: str
-    storage_url_suffix: str
-
-
-class UploadDefinitionList(BaseModel):
-    upload_definitions: List[UploadDefinition]
-
-
 class Upload:
     def __init__(self) -> None:
-        self.upload_definition_settings = get_upload_definition_settings()
+        self.upload_definition = Settings.file().upload_definition
 
     def upload_blob(self):
-        for upload_definition in self.upload_definition_settings:
-            health_check_url = upload_definition["health_check_url"]
-            paths = check_paths(upload_definition["paths"])
-            storage_account_name = upload_definition["storage_account_name"]
-            storage_container_name = upload_definition["storage_container_name"]
-            storage_url_suffix = upload_definition["storage_url_suffix"]
-
+        for definition in self.upload_definition:
             try:
                 logger.info(
-                    f"Connecting to storage account '{storage_account_name}' and storage container '{storage_container_name}'"
+                    f"Connecting to storage account '{definition.storage_account_name}' and storage container '{definition.storage_container_name}'"
                 )
-                storage_account_url = (
-                    f"https://{storage_account_name}.{storage_url_suffix}"
-                )
+                storage_account_url = f"https://{definition.storage_account_name}.{definition.storage_url_suffix}"
                 blob_service_client = BlobServiceClient(
                     storage_account_url, credential=DefaultAzureCredential()
                 )
 
                 logger.info(f"Getting files to upload")
-                files_to_upload = get_files_to_upload(paths)
+                files_to_upload = get_files_to_upload(check_paths(definition.paths))
                 logger.info(f"Found {len(files_to_upload)} file(s) to upload")
                 for file in files_to_upload:
                     blob_client = blob_service_client.get_blob_client(
-                        container=storage_container_name,
+                        container=definition.storage_container_name,
                         blob=file["name"],
                     )
 
@@ -187,9 +110,9 @@ class Upload:
                         upload_blob(file, blob_client)
 
             except Exception as e:
-                health_check(health_check_url + "/fail", str(e))
+                # health_check(definition.health_check_url + "/fail", str(e))
                 logger.error(e)
                 raise
 
-            else:
-                health_check(health_check_url)
+            # else:
+            #     health_check(definition.health_check_url)
